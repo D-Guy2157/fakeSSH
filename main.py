01cc6783e04faeb9ee5f3baa30cfc62afaa87fd7
Desktop D-Guy <ddcool535@gmail.com>
2025-03-25 17:27:53 -0400

    Fix and info logging addition

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import threading
 import logging
 
 # Configure Logging
-logging.basicConfig(filename="honeypot.log", level=logging.INFO, format="%(asctime)s - %(messgae)s")
+logging.basicConfig(filename="honeypot.log", level=logging.INFO, format="%(asctime)s - %(message)s")
+
+# Output
+def info(message):
+    print(message)
+    logging.info(message)
 
 # Client Handling
 def handle_client(client_socket, addr):
-    logging.info(f"Connection from {addr}")
+    info(f"Connection from {addr}")
 
     # Fake Banner
     client_socket.sendall(b"SSH-2.0-OpenSSH_9.9p2 Debian-1\n")
@@ -17,7 +22,7 @@ def handle_client(client_socket, addr):
     # Receive creds
     try:
         data = client_socket.recv(1024).decode("utf8").strip()
-        logging.info(f"Attempted login from {addr}: {data}")
+        info(f"Attempted login from {addr}: {data}")
     except Exception as e:
         logging.error(f"Error reciving data from {addr}: {e}")
 
@@ -25,12 +30,12 @@ def handle_client(client_socket, addr):
 
 
 # Honeypot
-def start_honeypot(host="0.0.0.0", port=22):
+def start_honeypot(host="0.0.0.0", port=2222):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((host, port))
     server.listen(5)
-    logging.info(f"SSH Honeypot listening on {host}: {port}")
+    info(f"SSH Honeypot listening on {host}:{port}")
 
     while True:
         client_socket, addr = server.accept()
